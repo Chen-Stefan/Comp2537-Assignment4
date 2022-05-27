e457d37412f1c47a0f3f1fc6a82754d936b2d0be
Stefan Chen <zchen158@my.bcit.ca>
2022-05-26 23:33:40 -0700

    Implement the game logic correctly for matching and unmatching cards

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let pairNum = null;
+let pokeNum = null;
 let cardNum = null;
 let cardArrayShuffled = [];
 let hasFlippedCard = false;
@@ -7,76 +7,107 @@ let secondCard = undefined;
 
 // Algorithm to shuffle an array
 function shuffle(array) {
-  let currentIndex = array.length,  randomIndex;
+  let currentIndex = array.length,
+    randomIndex;
   // While there remain elements to shuffle.
   while (currentIndex != 0) {
-
     // Pick a remaining element.
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
 
     // And swap it with the current element.
     [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
+      array[randomIndex],
+      array[currentIndex],
+    ];
   }
   return array;
 }
 
-function createGameGrid(pairNum, cardNum) {
-  $('#game_grid').empty();
+function createGameGrid(pokeNum, cardNum) {
+  $("#game_grid").empty();
   let indexArray = [];
-    // generate 3 or 6 unique random numbers between 1 and 898
-  while (indexArray.length < pairNum) {
+  while (indexArray.length < pokeNum) {
     let random = Math.floor(Math.random() * 898) + 1;
     if (indexArray.indexOf(random) === -1) indexArray.push(random);
   }
-  
-  if(pairNum == 3) {
-    cardArrayShuffled = shuffle([0, 1, 2, 0, 1, 2])
-  }else if(pairNum == 6) {
-    cardArrayShuffled = shuffle([0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5])
+
+  if (cardNum == 6) {
+    if (pokeNum == 2) {
+      cardArrayShuffled = shuffle([0, 1, 0, 1, 0, 1]);
+    } else {
+      cardArrayShuffled = shuffle([0, 1, 2, 0, 1, 2]);
+    }
+  }
+  if (cardNum == 12) {
+    if (pokeNum == 2) {
+      cardArrayShuffled = shuffle([0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]);
+    } else if (pokeNum == 3) {
+      cardArrayShuffled = shuffle([0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2]);
+    } else if (pokeNum == 4) {
+      cardArrayShuffled = shuffle([0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3]);
+    } else {
+      cardArrayShuffled = shuffle([0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5]);
+    }
   }
   for (i = 0; i < cardNum; i++) {
-    $('#game_grid').append(`
+    $("#game_grid").append(`
       <div class="card">
-        <img class="front_face" src="
-        https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${indexArray[cardArrayShuffled[i]]}.png" alt="">
+        <img id="${i + 1}" class="front_face" src="
+        https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
+          indexArray[cardArrayShuffled[i]]
+        }.png" alt="">
         <img class="back_face" src="./images/back_face.jpeg" alt="">
-      </div>`)
+      </div>`);
   }
 }
 
 function gameLogic() {
-  $('.card').on('click', function () {
-    $(this).toggleClass('flip')                   // toggleClass 是一个jQuery方法，如果有class, remove it; 如果没有, add it
+  $(".card").on("click", function () {
+    $(this).toggleClass("flip"); // toggleClass 是一个jQuery方法，如果有class, remove it; 如果没有, add it
 
-    if(!hasFlippedCard) {
-      firstCard = $(this).find('.front_face')[0];
+    if (!hasFlippedCard) {
+      firstCard = $(this).find(".front_face")[0];
       hasFlippedCard = true;
     } else {
-      secondCard = $(this).find('.front_face')[0];
-      if ($(firstCard).attr('src') == $(secondCard).attr('src')) {
-        console.log('You win')
-      }
+      secondCard = $(this).find(".front_face")[0];
+      hasFlippedCard = false;
+        // Check if cards match
+        if ($(`#${firstCard.id}`).attr("src") === $(`#${secondCard.id}`).attr("src")) {
+          console.log(`${firstCard.id}`, `${secondCard.id}`)
+          $(`#${firstCard.id}`).parent().off("click");
+          $(`#${secondCard.id}`).parent().off("click");
+        } else {
+          console.log(`${firstCard.id}`, `${secondCard.id}`)
+          setTimeout(() => {
+            $(`#${firstCard.id}`).parent().removeClass("flip");
+            $(`#${secondCard.id}`).parent().removeClass("flip");
+          }, 1000);
+        }
     }
-
-  })
+  });
 }
 
 function setup() {
-  $('input[type=radio][name=game_board]').change(function() {
-    if (this.value == 'small') {
-      createGameGrid(3, 6);
-      gameLogic();
+  $("input[type=radio][name=game_board]").change(function () {
+    $("#game_grid").empty();
+    if (this.value == "small") {
+      $("#pokemon_radio").hide();
+      $("input[type=radio][name=pokemon_size]").change(function () {
+        pokeNum = this.value;
+        createGameGrid(pokeNum, 6);
+        gameLogic();
+      });
     }
-    if (this.value == 'large') {
-      createGameGrid(6, 12);
-      gameLogic();
+    if (this.value == "large") {
+      $("#pokemon_radio").show();
+      $("input[type=radio][name=pokemon_size]").change(function () {
+        pokeNum = this.value;
+        createGameGrid(pokeNum, 12);
+        gameLogic();
+      });
     }
   });
-  
 }
-
-
 
 $(document).ready(setup);
