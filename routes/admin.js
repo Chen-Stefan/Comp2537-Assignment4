5e5d5786e2ad0a5ffaafd94b34e59b0ac581d987
Stefan Chen <zchen158@my.bcit.ca>
2022-05-29 15:36:07 -0700

    Implement admin dashboard interface

--- routes/admin.js
+++ routes/admin.js
@@ -11,14 +11,28 @@ router.use(bodyparser.urlencoded({
 
 // Loads all the users from DB and Rrenders the admin dashboard
 
-router.get('/', checkAuthenticatedAndAdmin, (req, res) => {
-  db.getAllUsers().then(function([rows, fields]) {
-      res.render('pages/admin', {
-          username: req.user.username,
-          users: rows
-      });
-  })
+router.get('/', checkAuthenticatedAndAdmin, async (req, res) => {
+  const users = await User.find();
+  res.render('pages/admin', {
+      username: req.user.username,
+      users: users
+  });
 })
+
+
+
+// Get user by ID
+
+router.get('/find/:id', async (req, res) => {
+  try {
+    await User.findById(req.params.id);
+    const {password, ...others} = user._doc;
+    res.status(200).json({others});
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
 
 
 
